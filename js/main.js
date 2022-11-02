1e709b2d41eb21373eb4676e8f02dbfd5f5fd84e
asemism <as29750as@gmail.com>
2022-11-03 01:50:48 +0300

    Missing bart in contact add

--- js/main.js
+++ js/main.js
@@ -328,7 +328,8 @@ function mealDisplay(mealData) {
     let tagList = ''
     let tagItem
     for (let i = 1; i <= 20; i++) {
-        if (mealData[`strIngredient${i}`]) { Ings += `<li class="my-3 mx-1 p-1 RliStyle rounded ">${mealData[`strMeasure${i}`]} ${mealData[`strIngredient${i}`]}</li>` }
+        if (mealData[`strIngredient${i}`])
+         { Ings += `<li class="my-3 mx-1 p-1 RliStyle rounded ">${mealData[`strMeasure${i}`]} ${mealData[`strIngredient${i}`]}</li>` }
     }
     if (tagItem != null) {
         tagItem = mealData.strTags.split(",")
@@ -373,67 +374,188 @@ function mealDisplay(mealData) {
 //////////////////////////////////
 // contact 
 
-$("#ContactUs").click(() => {
+$("#ContactUs").click(async () => {
+    await OCnav()
+    clean()
 
     $('#viewData').html(` 
     
-    <section id="contact" class="container w-75 m-auto text-center">  
-    <h2 class="text-light mb-5">Contact Us...</h2>
-    <div class="row">
-    <div class="col-md-6">
-        <div class="form-group">
-            <input type="text" class="form-control inputname" id="inputname"  placeholder="Enter Your Name">
-            <i class="bi bi-check-circle checkname"></i>
-            <div class="alert" id="alertname"></div>
+        <div class="text-center" id="contact-inputs">
+            <h2 class="text-white mb-5">Contac Us...</h2>
+            <div class="row contact-css">
+                <div class="col-md-6 ">
+                    <input id="userName" type="text" class="contact-style form-control w-100" placeholder="Enter Your Name">
+                    <div class="alert alertName mt-1 alert-danger ">
+                        Special Characters and Numbers not allowed
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <input id="userEmail" type="text" class="contact-style form-control w-100" placeholder="Enter Email">
+                    <div class="alert alertEmail mt-1 alert-danger ">
+                        Enter valid email. *Ex: xxx@yyy.zzz
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <input id="userPhone" type="text" class="contact-style form-control w-100" placeholder="Enter Phone">
+                    <div class="alert alertPhone mt-1 alert-danger ">
+                        It must be an Egyption valid phone number
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <input id="userAge" type="text" class="contact-style form-control w-100" placeholder="Enter Age">
+                    <div class="alert alertAge mt-1 alert-danger ">
+                        Enter valid Age between 10-100 years
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <input id="userPassword" type="password" class="contact-style form-control w-100" placeholder="Enter Password">
+                    <div class="alert alertPassword mt-1 alert-danger ">
+                        Enter valid password *Minimum eight characters, at least one letter and one number:*
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <input id="userRePassword" type="password" class="contact-style form-control w-100" placeholder="Enter RePassword">
+                    <div class="alert alertRePassword mt-1 alert-danger ">
+                        Enter valid Repassword
+                    </div>
+                </div>
+            </div>
+            <button class="btn btn-outline-danger mt-4" id="btn" disabled>submit</button>
         </div>
-    </div>
-    <div class="col-md-6">
-        <div class="form-group">
-            <input type="email" class ="form-control inputemail" placeholder="Enter Your Email">
-            <i class="bi bi-check-circle checkemail"></i>
-            <div class="alert" id="alertemail"></div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="form-group">
-            <input  class="form-control inputphone"  placeholder="Enter Your Phone">
-            <i class="bi bi-check-circle checkphone"></i>
-            <div class="alert" id="alertphone"></div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="form-group">
-            <input  class="form-control inputage" id="age" placeholder="Enter Your Age">
-            <i class="bi bi-check-circle checkage"></i>
-            <div class="alert" id="alertage"></div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="form-group">
-            <input type="password" class="form-control inputpassword" id="pasword" placeholder="Enter Your Pasword">
-            <i class="bi bi-check-circle checkpassword"></i>
-            <div class="alert" id="alertpasword"></div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="form-group">
-            <input type="password" class="form-control inputrepassword" id="repasword" placeholder="Enter Repasword">
-            <i class="bi bi-check-circle checkrepassword"></i>
-            <div class="alert" id="alertrepassword"></div>
-        </div>
-    </div>
-    </div>	
-      <button type="submit" id="submitBtn" class="btn btn-outline-success mt-5 ">Submit</button>
+    `)
+
+    let userNameInput = false
+    let userEmailInput = false
+    let userPhoneInput = false
+    let userAgeInput = false
+    let userPasswordInput = false
+    let userRePasswordInput = false
+
+
+
+    $('#userName').keyup( 
+    ()=> {
+        return userNameCall (/^[a-z]{1,}$/ig.test($('#userName').val())) 
+    })
+
+    $('#userEmail').keyup( 
+        ()=> {
+        return userEmailCall (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($('#userEmail').val()))
+    })
+
+    $('#userPhone').keyup( 
+        ()=> {
+        return userPhoneCall (/^(002)?(01)[0125]{1}[0-9]{8}$/.test($('#userPhone').val()))
+    })
+
+    $('#userAge').keyup( 
+        ()=> {
+        return userAgeCall (/(^[1-9]{1}[0-9]{1}|100)$/.test($('#userAge').val()))
+    })
+
+    $('#userPassword').keyup( 
+        ()=> {
+        return userPasswordCall (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test($('#userPassword').val()))
+    })
+
+    $('#userRePassword').keyup( 
+        ()=> {
+        return userRePasswordCall ($('#userPassword').val() == $('#userRePassword').val())
+    })
+
+    function ifTrue (){
+        if (userNameInput && userEmailInput && userPhoneInput && userAgeInput && userPasswordInput && userRePasswordInput) {
+            $('#btn').removeAttr('disabled')
+        } else {
+            $('#btn').attr('disabled', 'true')
+        }
+
+    }
     
-    </section> `)
+    
+    function userNameCall (x){
+        if (x == true) {
+            $('#userName').addClass('is-valid')
+            $('#userName').removeClass('is-invalid')
+            $('.alertName').removeClass('d-block')
+            userNameInput = true
+            ifTrue ()
+        } else if(x == false) {
+            $('#userName').addClass('is-invalid')
+            $('#userName').removeClass('is-valid')
+            $('.alertName').addClass('d-block')
+        }
 
-
-
+    }
+    
+    function userEmailCall (x){
+        if (x == true) {
+            $('#userEmail').addClass('is-valid')
+            $('#userEmail').removeClass('is-invalid')
+            $('.alertEmail').removeClass('d-block')
+            userEmailInput = true
+            ifTrue ()
+        } else if(x == false)  {
+            $('#userEmail').addClass('is-invalid')
+            $('#userEmail').removeClass('is-valid')
+            $('.alertEmail').addClass('d-block')
+        }
+    }
+    function userPhoneCall (x){
+    if (x == true) {
+            $('#userPhone').addClass('is-valid')
+            $('#userPhone').removeClass('is-invalid')
+            $('.alertPhone').removeClass('d-block')
+            userPhoneInput = true
+            ifTrue ()
+        } else if(x == false)   {
+            $('#userPhone').addClass('is-invalid')
+            $('#userPhone').removeClass('is-valid')
+            $('.alertPhone').addClass('d-block')
+            userPhoneInput = false
+        }
+    }
+    function userAgeCall (x){
+    if (x == true) {
+            $('#userAge').addClass('is-valid')
+            $('#userAge').removeClass('is-invalid')
+            $('.alertAge').removeClass('d-block')
+            userAgeInput = true
+            ifTrue ()
+        } else if(x == false)  {
+            $('#userAge').addClass('is-invalid')
+            $('#userAge').removeClass('is-valid')
+            $('.alertAge').addClass('d-block')
+            userAgeInput = false
+        }
+    }
+    function userPasswordCall (x){
+    if (x == true) {
+            $('#userPassword').addClass('is-valid')
+            $('#userPassword').removeClass('is-invalid')
+            $('.alertPassword').removeClass('d-block')
+            userPasswordInput = true
+            ifTrue ()
+        } else if(x == false)  {
+            $('#userPassword').addClass('is-invalid')
+            $('#userPassword').removeClass('is-valid')
+            $('.alertPassword').addClass('d-block')
+            userPasswordInput = false
+        }
+    }
+    function userRePasswordCall (x){
+    if (x == true) {
+            $('#userRePassword').addClass('is-valid')
+            $('#userRePassword').removeClass('is-invalid')
+            $('.alertRePassword').removeClass('d-block')
+            userRePasswordInput = true
+            ifTrue ()
+        } else if(x == false)  {
+            $('#userRePassword').addClass('is-invalid')
+            $('#userRePassword').removeClass('is-valid')
+            $('.alertRePassword').addClass('d-block')
+            userRePasswordInput = false
+        }
+    }
 })
-
-
-
-
-
-
 
